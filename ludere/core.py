496251d9c34b9998d20d2806b9e708d9cd7ae303
Vlad Smirnov <gnudeb0@gmail.com>
2022-01-11 23:11:41 +0200

    Nested deps

--- ludere/core.py
+++ ludere/core.py
@@ -1,5 +1,6 @@
 from typing import List, Type, Set, Any, Dict, Optional, TypeVar
 
+from ludere.reflection import resolve_constructor_parameter_types
 
 T = TypeVar("T")
 
@@ -15,10 +16,30 @@ class Ludere:
         return cls
 
     def resolve(self):
-        for cls in list(self._pending_resolution):
-            instance = cls()
-            self._resolved_beans[cls] = instance
-            self._pending_resolution.remove(cls)
+        while len(self._pending_resolution) > 0:
+            for cls in list(self._pending_resolution):
+                self._attempt_to_resolve(cls)
 
     def get_bean(self, cls: Type[T]) -> Optional[T]:
         return self._resolved_beans.get(cls)
+
+    def _attempt_to_instantiate(self, cls: Type[T]) -> Optional[T]:
+        constructor_dependencies = resolve_constructor_parameter_types(cls)
+
+        if not all(self._is_resolved(dep_cls) for dep_cls in constructor_dependencies):
+            return None
+
+        constructor_arguments = [self.get_bean(dep_cls) for dep_cls in constructor_dependencies]
+        return cls(*constructor_arguments)
+
+    def _is_resolved(self, cls: Type):
+        return cls in self._resolved_beans.keys()
+
+    def _attempt_to_resolve(self, cls: Type):
+        instance = self._attempt_to_instantiate(cls)
+
+        if instance is None:
+            return
+
+        self._resolved_beans[cls] = instance
+        self._pending_resolution.remove(cls)
--- tests/test_basic_features.py
+++ tests/test_basic_features.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from ludere.core import Ludere
 
 
@@ -16,3 +18,22 @@ def test_can_instantiate_class_without_dependencies():
 
     instance = ludere.get_bean(SimpleClass)
     assert type(instance) == SimpleClass
+
+
+def test_can_instantiate_nested_dependency():
+    ludere = Ludere()
+
+    @ludere.register
+    class Child:
+        pass
+
+    @ludere.register
+    @dataclass
+    class Parent:
+        child: Child
+
+    ludere.resolve()
+
+    assert ludere.get_bean(Child) is not None
+    assert ludere.get_bean(Parent) is not None
+    assert type(ludere.get_bean(Parent).child) is Child
